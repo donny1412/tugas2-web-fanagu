cb64a49444b96d6afdd16c1081ae6cbd3efba357
Hana Ghaliyah <hanaghaliyahazhar@gmail.com>
2019-10-20 18:00:45 +0700

    032

---
+++ tourism.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2019 at 12:59 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tourism`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotel`
+--
+
+CREATE TABLE IF NOT EXISTS `hotel` (
+  `no` int(100) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `datefrom` date NOT NULL,
+  `dateto` date NOT NULL,
+  `guest` int(100) NOT NULL,
+  `children` int(100) NOT NULL,
+  PRIMARY KEY (`no`),
+  UNIQUE KEY `nama` (`nama`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE IF NOT EXISTS `signup` (
+  `no` int(100) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`no`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `signup`
+--
+
+INSERT INTO `signup` (`no`, `username`, `email`, `password`) VALUES
+(1, 'test', 'test', 'test'),
+(2, 'hanaghaliyah', 'hanaghaliyahazhar@gmail.com', 'hana123'),
+(3, 'hanaghz_', 'hana%gmail.com', 'hanaaaa'),
+(7, 'hanagha', 'hanaghaloyah@gmail.com', '12345'),
+(8, 'febi', 'febi@gmail.com', 'febi123'),
+(9, 'Bayuganteng', 'bayu.trianasa18@gmail.com', 'bayutrianayasa');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

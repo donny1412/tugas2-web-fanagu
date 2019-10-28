d53d5eb5ff8c0abeaf546b217d1818647feb6016
Hana Ghaliyah <hanaghaliyahazhar@gmail.com>
2019-10-28 18:16:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 
         <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+            <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
             <li class="nav-item"><a href="places.php" class="nav-link">Destinasi Wisata</a></li>
             <li class="nav-item"><a href="hotel.php" class="nav-link">Hotel</a></li>
             <li class="nav-item"><a href="Tour Guide.php" class="nav-link">Tour Guide</a></li> 

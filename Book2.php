7942af1a9a7096ace81e2fee3a16773dca4101a1
fazamurtadho <50489529+Krauszie@users.noreply.github.com>
2019-10-29 18:10:13 +0700

    Add files via upload

---
+++ Book2.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mlaku.id</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html">Mlaku.id</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="places.html" class="nav-link">Destinasi Wisata</a></li>
+	          <li class="nav-item active"><a href="hotel.html" class="nav-link">Hotel</a></li>
+	          <li class="nav-item"><a href="Tour Guide.html" class="nav-link">Tour Guide</a></li>
+	          <li class="nav-item"><a href="transportasi.html" class="nav-link">Transportasi</a></li>
+	          <li class="nav-item"><a href="login.php" class="nav-link">Log Out</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+    <div class="hero-wrap js-fullheight" style="background-image: url('images/thumb.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>Hotel</span></p>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Hotels</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center mb-1 pb-1">
+          <div class="col-md-7 heading-section text-center ftco-animate">
+            <h2 class="mb-1">BOOKING SECTION</h2>
+          </div>
+        </div>
+      </section>
+
+
+          		<div class="col-md-12 hotel-single ftco-animate mb-5 mt-4">
+          			<div class="fields">
+          				<div class="row">
+          					<div class="col-md-6">
+				              <div class="form-group">
+				                <input type="text" class="form-control" placeholder="Name">
+				              </div>
+
+			              </div>
+			              <div class="col-md-6">
+				              <div class="form-group">
+				                <input type="text" class="form-control" placeholder="Email">
+				              </div>
+			              </div>
+			              <div class="col-md-6">
+				              <div class="form-group">
+				                <input type="text" class="form-control checkin_date" placeholder="Date from">
+				              </div>
+			              </div>
+			              <div class="col-md-6">
+				              <div class="form-group">
+				                <input type="text" class="form-control checkout_date" placeholder="Date to">
+				              </div>
+				            </div>
+				            <div class="col-md-6">
+					            <div class="form-group">
+				                <div class="select-wrap one-third">
+			                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+			                    <select name="" id="" class="form-control" placeholder="Guest">
+			                      <option value="0">Guest</option>
+			                      <option value="1">1</option>
+			                      <option value="2">2</option>
+			                      <option value="3">3</option>
+			                      <option value="4">4</option>
+			                    </select>
+			                  </div>
+				              </div>
+			              </div>
+			              <div class="col-md-6">
+					            <div class="form-group">
+				                <div class="select-wrap one-third">
+			                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+			                    <select name="" id="" class="form-control" placeholder="Children">
+			                      <option value="0">Children</option>
+			                      <option value="1">1</option>
+			                      <option value="2">2</option>
+			                      <option value="3">3</option>
+			                      <option value="4">4</option>
+			                    </select>
+			                  </div>
+				              </div>
+			              </div>
+				            <div class="col-md-12">
+				              <div class="form-group">
+				                  <h3><a href="hotel-tanijiwo.html" input type="submit" class="btn btn-primary py-3">Book And Checkout!</a></h3>
+				              </div>
+			              </div>
+		              </div>
+		            </div>
+          		</div>
+
+
+		<section class="ftco-section-parallax">
+      <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+              <h2>Subcribe to our Newsletter</h2>
+              <p>Info</p>
+              <div class="row d-flex justify-content-center mt-5">
+                <div class="col-md-8">
+                  <form action="#" class="subscribe-form">
+                    <div class="form-group d-flex">
+                      <input type="text" class="form-control" placeholder="Enter email address">
+                      <input type="submit" value="Subscribe" class="submit px-3">
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Mlaku.id</h2>
+              <p>Memudahkan Liburan Anda.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Surabaya, Indonesia</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">0355 332750</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">mlaku@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+      </div>
+    </footer>
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>

38125a2c54b89ecf635455a73c8b21acdab99d22
fazamurtadho <50489529+Krauszie@users.noreply.github.com>
2019-10-29 18:23:51 +0700

    Update hotel.php

--- hotel.php
+++ hotel.php
@@ -180,7 +180,7 @@
 
               <div class="col-sm col-md-6 col-lg-4 ftco-animate">
                 <div class="destination">
-                  <a href="#" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/hotel/bromo_terrace/bromoterrace.jpg);">
+                  <a href="MT-hotel.php" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/hotel/bromo_terrace/bromoterrace.jpg);">
                     <div class="icon d-flex justify-content-center align-items-center">
                       <span class="icon-link"></span>
                     </div>

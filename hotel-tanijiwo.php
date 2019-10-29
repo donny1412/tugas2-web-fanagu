40826dd05aa8dae2342d59cb26a45f8580ad4457
fazamurtadho <50489529+Krauszie@users.noreply.github.com>
2019-10-29 17:52:45 +0700

    Add files via upload

---
+++ hotel-tanijiwo.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mlaku.id</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html">Mlaku.id</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="places.html" class="nav-link">Destinasi Wisata</a></li>
+	          <li class="nav-item active"><a href="hotel.html" class="nav-link">Hotel</a></li>
+	          <li class="nav-item"><a href="login.php" class="nav-link">Log Out</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+    <div class="hero-wrap js-fullheight" style="background-image: url('images/thumb.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>Hotel</span></p>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Hotels</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+		<section class="ftco-section ftco-degree-bg">
+      <div class="container">
+        <div class="row">
+        	<div class="col-lg-3 sidebar">
+        		<div class="sidebar-wrap ftco-animate">
+        			<h3 class="heading mb-4">Find City</h3>
+        			<form action="#">
+        				<div class="fields">
+		              <div class="form-group">
+		                <div class="select-wrap one-third">
+	                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+	                    <select name="" id="" class="form-control" placeholder="Keyword search">
+	                      <option value="">Select Location</option>
+	                      <option value="">Bandung</option>
+	                      <option value="">Jakarta</option>
+	                      <option value="">Malang</option>
+	                      <option value="">Yogyakarta</option>
+	                    </select>
+	                  </div>
+		              </div>
+		              <div class="form-group">
+		               <a href="hotel.html"input type="submit" class="btn btn-primary py-3 px-5">Search</a>
+		              </div>
+		            </div>
+	            </form>
+        		</div>
+          </div>
+          <div class="col-lg-9">
+          	<div class="row">
+          		<div class="col-md-12 ftco-animate">
+          			<div class="single-slider owl-carousel">
+          				<div class="item">
+          					<div class="hotel-img" style="background-image: url(images/hotel/tani_jiwo/tanijiwo.jpg);"></div>
+          				</div>
+          				<div class="item">
+          					<div class="hotel-img" style="background-image: url(images/hotel/tani_jiwo/tanijiwo2.jpg);"></div>
+          				</div>
+          				<div class="item">
+          					<div class="hotel-img" style="background-image: url(images/hotel/tani_jiwo/tanijiwo3.jpg);"></div>
+          				</div>
+          			</div>
+          		</div>
+          		<div class="col-md-12 hotel-single mt-4 mb-5 ftco-animate">
+          			<span>Our Best hotels &amp; Rooms</span>
+          			<h2>Tani Jiwo Hostel</h2>
+          			<p class="rate mb-3">
+          				<span class="loc"><a href="#"><i class="icon-map"></i> Jl. Raya Dieng No.31, Simpangan, Dieng Kulon, Batur, Banjarnegara, Jawa Tengah 53456</a></span></p>
+                  <p>
+          				<span class="star">
+    							<i class="icon-star"></i>
+    							<i class="icon-star"></i>
+    							<i class="icon-star"></i>
+    							<i class="icon-star"></i>
+    							<i class="icon-star"></i>
+    							10 Rating</span>
+    						</p>
+    						<p>Info 1</p>
+    						<div class="d-md-flex mt-5 mb-5">
+    							<ul>
+	    							<li>Pros</li>
+	    							<li>1</li>
+	    							<li>2 </li>
+	    							<li>3</li>
+	    						</ul>
+	    						<ul class="ml-md-5">
+	    							<li>Cons</li>
+	    							<li>1</li>
+	    							<li>2</li>
+	    							<li>3</li>
+	    						</ul>
+    						</div>
+    						<p>Info 2</p>
+          		</div>
+          		<div class="col-md-12 hotel-single ftco-animate mb-5 mt-4">
+          			<h4 class="mb-4">Take A Tour</h4>
+          			<div class="block-16">
+		              <figure>
+		                <img src="images/hotel/tani_jiwo/tanijiwo.jpg" alt="Image placeholder" class="img-fluid">
+		                <a href="https://vimeo.com/45830194" class="play-button popup-vimeo"><span class="icon-play"></span></a>
+		              </figure>
+		            </div>
+          		</div>
+          		<div class="col-md-12 hotel-single ftco-animate mb-5 mt-4">
+          			<h4 class="mb-4">Our Rooms</h4>
+          			<div class="row">
+          				<div class="col-md-4">
+				    				<div class="destination">
+				    					<a href="hotel-tanijiwo.html" class="img img-2" style="background-image: url(images/hotel/tani_jiwo/tanijiwo2.jpg);"></a>
+				    					<div class="text p-3 ">
+				    						<div class="d-flex">
+				    							<div class="one">
+						    						<h3>Twin Bed</h3>
+                          </div>
+					    						<p><div class="two">
+					    							<span class="price per-price">IDR 300.000<br><small>/night</small></span>
+				    							</div>
+				    						 </div>
+
+				    						<p>Info</p>
+				    						<hr>
+				    						<p class="bottom-area d-flex">
+				    							<span class="ml-auto"><a href="Book2.html">Book Now</a></span>
+				    						</p>
+				    					</div>
+				    				</div>
+				    			</div>
+				    			<div class="col-md-4">
+				    				<div class="destination">
+				    					<a href="hotel-single.html" class="img img-2" style="background-image: url(images/hotel/tani_jiwo/tanijiwo3.jpg);"></a>
+				    					<div class="text p-3">
+				    						<div class="d-flex">
+				    							<div class="one">
+						    						<h3>Bunk Bed</h3>
+					    						</div>
+					    						<div class="two">
+					    							<span class="price per-price">IDR 150.000<br><small>/night</small></span>
+				    							</div>
+				    						</div>
+				    						<p>Info</p>
+				    						<hr>
+				    						<p class="bottom-area d-flex">
+				    							<span class="ml-auto"><a href="Book-Bunk.html">Book Now</a></span>
+				    						</p>
+				    					</div>
+				    				</div>
+				    			</div>
+				    			<div class="col-md-4">
+				    				<div class="destination">
+				    					<a href="hotel-single.html" class="img img-2" style="background-image: url(images/hotel/tani_jiwo/tanijiwo4.jpg);"></a>
+				    					<div class="text p-3">
+				    						<div class="d-flex">
+				    							<div class="one">
+						    						<h3><a href="hotel-single.html">Double Bed</a></h3>
+					    						</div>
+					    						<div class="two">
+					    							<span class="price per-price">IDR 400.000<br><small>/night</small></span>
+				    							</div>
+				    						</div>
+				    						<p>Infos</p>
+				    						<hr>
+				    						<p class="bottom-area d-flex">
+				    							<span class="ml-auto"><a href="Book2.html">Book Now</a></span>
+				    						</p>
+				    					</div>
+				    				</div>
+				    			</div>
+          			</div>
+          		</div>
+	           </div>
+
+          		<div class="col-md-12 hotel-single ftco-animate mb-5 mt-5">
+          			<h4 class="mb-4">Related Hotels</h4>
+          			<div class="row">
+          				<div class="col-md-4">
+				    				<div class="destination">
+				    					<a href="hotel-single.html" class="img img-2" style="background-image: url(images/hotel/jiwa_jawa/jiwajawa.jpg);"></a>
+				    					<div class="text p-3">
+				    						<div class="d-flex">
+				    							<div class="one">
+						    						<h3><a href="hotel-single.html">Jiwa Jawa Resort</a></h3>
+						    						<p class="rate">
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<span>10 Rating</span>
+						    						</p>
+					    						</div>
+				    						</div>
+				    						<p>Rp. Sekian</p>
+				    						<hr>
+				    						<p class="bottom-area d-flex">
+				    							<span><i class="icon-map-o"></i> Probolinggo, Jawa Timur</span>
+				    							<span class="ml-auto"><a href="#">Book</a></span>
+				    						</p>
+				    					</div>
+				    				</div>
+				    			</div>
+				    			<div class="col-md-4">
+				    				<div class="destination">
+				    					<a href="hotel-single.html" class="img img-2" style="background-image: url(images/hotel/bromo_terrace/bromoterrace.jpg);"></a>
+				    					<div class="text p-3">
+				    						<div class="d-flex">
+				    							<div class="one">
+						    						<h3><a href="hotel-single.html">Bromo Terrace</a></h3>
+                            <p class="rate">
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<span>10 Rating</span>
+						    						</p>
+					    						</div>
+				    						</div>
+				    						<p>Rp. Sekian</p>
+				    						<hr>
+				    						<p class="bottom-area d-flex">
+				    							<span><i class="icon-map-o"></i> Probolinggo, Jawa Timur</span>
+				    							<span class="ml-auto"><a href="#">Book</a></span>
+				    						</p>
+				    					</div>
+				    				</div>
+				    			</div>
+				    			<div class="col-md-4">
+				    				<div class="destination">
+				    					<a href="hotel-single.html" class="img img-2" style="background-image: url(images/hotel/intercontinental/intercontinental.jpg);"></a>
+				    					<div class="text p-3">
+				    						<div class="d-flex">
+				    							<div class="one">
+						    						<h3><a href="hotel-single.html">InterContinental Bandung Dago Pakar</a></h3>
+						    						<p class="rate">
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<i class="icon-star"></i>
+						    							<span>10 Rating</span>
+						    						</p>
+					    						</div>
+				    						</div>
+				    						<p>Rp. Sekian</p>
+				    						<hr>
+				    						<p class="bottom-area d-flex">
+				    							<span><i class="icon-map-o"></i> Lembang, Bandung</span>
+				    							<span class="ml-auto"><a href="#">Book</a></span>
+				    						</p>
+				    					</div>
+				    				</div>
+				    			</div>
+          			</div>
+          		</div>
+
+          	</div>
+          </div> <!-- .col-md-8 -->
+        </div>
+      </div>
+    </section> <!-- .section -->
+
+		<section class="ftco-section-parallax">
+      <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+              <h2>Subcribe to our Newsletter</h2>
+              <p>Info</p>
+              <div class="row d-flex justify-content-center mt-5">
+                <div class="col-md-8">
+                  <form action="#" class="subscribe-form">
+                    <div class="form-group d-flex">
+                      <input type="text" class="form-control" placeholder="Enter email address">
+                      <input type="submit" value="Subscribe" class="submit px-3">
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Mlaku.id</h2>
+              <p>Memudahkan Liburan Anda.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Surabaya, Indonesia</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">0355 332750</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">mlaku@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+      </div>
+    </footer>
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>

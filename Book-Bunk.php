adbe5e6845dce2e5a5199eb28de26aaffc48caa3
fazamurtadho <50489529+Krauszie@users.noreply.github.com>
2019-10-30 14:21:41 +0700

    Update Book-Bunk.php

--- Book-Bunk.php
+++ Book-Bunk.php
@@ -31,18 +31,18 @@
 
 	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.html">Mlaku.id</a>
+	      <a class="navbar-brand" href="index.php">Mlaku.id</a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="places.html" class="nav-link">Destinasi Wisata</a></li>
-	          <li class="nav-item active"><a href="hotel.html" class="nav-link">Hotel</a></li>
-	          <li class="nav-item"><a href="Tour Guide.html" class="nav-link">Tour Guide</a></li>
-	          <li class="nav-item"><a href="transportasi.html" class="nav-link">Transportasi</a></li>
+	          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="places.php" class="nav-link">Destinasi Wisata</a></li>
+	          <li class="nav-item active"><a href="hotel.php" class="nav-link">Hotel</a></li>
+	          <li class="nav-item"><a href="Tour Guide.php" class="nav-link">Tour Guide</a></li>
+	          <li class="nav-item"><a href="transportasi.php" class="nav-link">Transportasi</a></li>
 	          <li class="nav-item"><a href="login.php" class="nav-link">Log Out</a></li>
 	        </ul>
 	      </div>

5ec5827752083dafc7d3561bd28bf4fb8c5c8238
donny1412 <55181699+donny1412@users.noreply.github.com>
2019-11-05 16:21:01 +0700

    Update login.php

--- login.php
+++ login.php
@@ -57,9 +57,9 @@ if($cek > 0){
 		
 		<p class="tulisan_login">Mlaku.id</p>
 		<?php if ($msg != "") { ?> <div class="alert alert-warning" role="alert"><?php echo $msg;?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <!--  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
-                    </button>
+                    </button> -->
                 </div><?php }?>
 		<form method="POST" action="login.php">
 			<label>Email</label>
